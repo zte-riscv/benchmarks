8a974f8cfb3c1ee6e29ea6c4e4fdf68d4fa82d2f
Qiuweihong <953950914@qq.com>
2025-11-12 17:12:33 +0800

    add biogo-krishna

--- sweet/run_riscv64.bash
+++ sweet/run_riscv64.bash
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+# biogo-krishna
+./prebuilt_bin/riscv64/sweet run -run biogo-krishna -assets-dir ./prebuilt_assets --prebuilt-binary-dir ./prebuilt_bin/riscv64/biogo-krishna/myconfig ./config_riscv64.toml
+
+# biogo-igor
 ./prebuilt_bin/riscv64/sweet run -run biogo-igor -assets-dir ./prebuilt_assets --prebuilt-binary-dir ./prebuilt_bin/riscv64/biogo-igor/myconfig ./config_riscv64.toml
