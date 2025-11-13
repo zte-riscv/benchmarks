6182db053e680d86427fee2813c875e10488a02d
Qiuweihong <953950914@qq.com>
2025-11-13 09:25:41 +0800

    add gopher-lua

---
+++ sweet/prebuilt_assets/gopher-lua/k-nucleotide.lua
@@ -0,0 +1,39 @@
+-- The Computer Language Benchmarks Game
+-- https://salsa.debian.org/benchmarksgame-team/benchmarksgame/
+-- contributed by Mike Pall
+
+local function kfrequency(seq, freq, k, frame)
+  local sub = string.sub
+  local k1 = k - 1
+  for i=frame,string.len(seq)-k1,k do
+    local c = sub(seq, i, i+k1)
+    freq[c] = freq[c] + 1
+  end
+end
+
+local function freqdefault()
+  return 0
+end
+
+function count(seq, frag)
+  local k = string.len(frag)
+  local freq = setmetatable({}, { __index = freqdefault })
+  for frame=1,k do kfrequency(seq, freq, k, frame) end
+  io.write(freq[frag] or 0, "\t", frag, "\n")
+end
+
+function frequency(seq, k)
+  local freq = setmetatable({}, { __index = freqdefault })
+  for frame=1,k do kfrequency(seq, freq, k, frame) end
+  local sfreq, sn = {}, 1
+  for c,v in pairs(freq) do sfreq[sn] = c; sn = sn + 1 end
+  table.sort(sfreq, function(a, b)
+    local fa, fb = freq[a], freq[b]
+    return fa == fb and a > b or fa > fb
+  end)
+  sum = string.len(seq)-k+1
+  for _,c in ipairs(sfreq) do
+    io.write(string.format("%s %0.3f\n", c, (freq[c]*100)/sum))
+  end
+  io.write("\n")
+end
--- sweet/run_riscv64.bash
+++ sweet/run_riscv64.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# gopher-lua
+./prebuilt_bin/riscv64/sweet run -run gopher-lua -assets-dir ./prebuilt_assets --prebuilt-binary-dir ./prebuilt_bin/riscv64/gopher-lua/myconfig ./config_riscv64.toml
+
 # biogo-krishna
 ./prebuilt_bin/riscv64/sweet run -run biogo-krishna -assets-dir ./prebuilt_assets --prebuilt-binary-dir ./prebuilt_bin/riscv64/biogo-krishna/myconfig ./config_riscv64.toml
 
